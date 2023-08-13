343eae926b79ba2ff44c760391b1f7dfc62a5144
Bharath Babu <bharathbabu68@gmail.com>
2023-08-13 19:30:15 +0530

    drag & drop image upload for minting

--- frontend/gift-some-crypto/src/components/Gift.jsx
+++ frontend/gift-some-crypto/src/components/Gift.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import { Container, Button, Row, Col, Form } from "react-bootstrap";
 import NavBar from "./NavBar";
 import Web3Modal from 'web3modal'
 import { ethers } from 'ethers'
+import { FileUploader } from "react-drag-drop-files";
 
 
 
 const Gift = () => {
 
     const [account, setAccount] = useState('')
+    const [connectWalletStatus, setConnectWalletStatus] = useState('Connect Wallet')
     const [accountTrimmed, setAccountTrimmed] = useState('')
     const [accountBalance, setAccountBalance] = useState('')
+
+
+    const [file, setFile] = useState(null);
+    const [giftCardRecipientAddress, setGiftCardRecipientAddress] = useState("")
+    const [giftCardValue, setGiftCardValue] = useState("")
+
+
+    const handleChange = (file) => {
+        const imgUrl = URL.createObjectURL(file)
+        setFile(imgUrl);
+    };
  
     const web3Modal = new Web3Modal({
         theme: "dark",
         network: "mainnet", // optional
         cacheProvider: true, // optional
       });
+
+      const fileTypes = ["JPG", "PNG", "GIF"];
 
 
   async function connectWallet() {
@@ -29,9 +44,12 @@ const Gift = () => {
     setAccountTrimmed(account_trimmed)
     const network = await provider.getNetwork()
     const balance = await provider.getBalance(account)
-    const balanceinEther =  ethers.utils.formatEther(balance)
+    const balanceinEther =  Number(ethers.utils.formatEther(balance))
+    // round it to two decimals
+    const balanceinEtherFormatted = balanceinEther.toFixed(2)
     console.log(balanceinEther)
-    setAccountBalance(balanceinEther)
+    setAccountBalance(balanceinEtherFormatted)
+    setConnectWalletStatus("Connected")
     console.log(account)
   }
 
@@ -39,18 +57,45 @@ const Gift = () => {
     <>
       <div fluid className="home-page">
         <NavBar />
-        <Container fluid style={{ marginTop: "2%", textAlign: "center" }}>
+        <Container fluid style={{ marginTop: "2%", textAlign: "" }}>
           <Row>
-            <Col md={9}></Col>
+            <Col md={5}>
+            <h2 style={{ fontWeight: "bold", marginBottom:"2%"}}>Mint Gift Card</h2>
+            <p>Connected Address: {accountTrimmed} Balance: {String(accountBalance)} ETH</p>
+            </Col>
+            <Col md={4}>
+            </Col>
             <Col style={{ textAlign: "right" }} md={3}>
               <Button onClick={connectWallet} variant="dark">
-                Connect Wallet
+                {connectWalletStatus}
               </Button>
             </Col>
           </Row>
-          <h2 style={{ fontWeight: "bold" }}>Mint Gift Card</h2>
-          <p>Connected Address: {accountTrimmed}</p>
-          <p>Balance: {String(accountBalance)} ETH</p>
+          <Row style={{marginTop:"3%"}}>
+            <Col md={6}>
+          <p>Select a cover image for your gift card:</p>
+          <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+          <br />
+
+          <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Enter receipient address to which you'd like to gift </Form.Label>
+        <Form.Control onChange={(e)=>setGiftCardRecipientAddress(e.target.value)} type="text" placeholder="0x80d87.......F84E181" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Enter gift card value in ETH </Form.Label>
+        <Form.Control onChange={(e)=>setGiftCardValue(e.target.value)} type="text" placeholder="0.1" />
+        </Form.Group>
+        </Form>
+
+            </Col>
+            <Col style={{textAlign:"right", padding:"3%", paddingTop:'0%'}} md={6}>
+        <h5>Gift Card Preview</h5>
+          <img width="256" height="256" src={file}></img>
+          <p style={{marginTop:"2%", fontSize:"0.8rem"}}>Recipient Address: {giftCardRecipientAddress}</p>
+          <p style={{fontSize:"0.8rem"}}>Gift Card Value: {giftCardValue}</p>
+            </Col>
+          </Row>
         </Container>
       </div>
     </>
--- frontend/gift-some-crypto/src/components/NavBar.jsx
+++ frontend/gift-some-crypto/src/components/NavBar.jsx
@@ -7,8 +7,8 @@ const NavBar = () => {
     <Container fluid>
       <Navbar.Brand href="/">Gift Some Crypto</Navbar.Brand>
       <Nav className="me-auto">
-        <Nav.Link href="/mint">Mint Gift</Nav.Link>
-        <Nav.Link href="/redeem">Redeem Gift</Nav.Link>
+        <Nav.Link href="/mint">Mint </Nav.Link>
+        <Nav.Link href="/redeem">Redeem </Nav.Link>
       </Nav>
     </Container>
   </Navbar>
