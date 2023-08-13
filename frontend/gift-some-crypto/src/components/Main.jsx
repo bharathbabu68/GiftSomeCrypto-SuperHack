62493ba92f6d4521d388d10e6f48fb1ddfbd5732
Bharath Babu <bharathbabu68@gmail.com>
2023-08-13 16:34:31 +0530

    react components base creation & packages

---
+++ frontend/gift-some-crypto/src/components/Main.jsx
@@ -0,0 +1,25 @@
+import {  BrowserRouter,  Switch, Route, Link, } from "react-router-dom";
+import {BrowserView, MobileView} from 'react-device-detect';
+import Home from "./Home";
+import Gift from "./Gift";
+import Redeem from "./Redeem";
+import PageNotFound from "./PageNotFound";
+
+const Main = () => {
+    return (
+      <>
+      <BrowserRouter>
+          <Switch>
+          <Route path="/" exact component={Home}/>
+          <BrowserView>
+            <Route path="/gift" exact component={Gift} />
+            <Route path="/redeem" exact component={Redeem}/>
+            <Route path="/"  component={PageNotFound}/>  
+            </BrowserView>        
+          </Switch>
+      </BrowserRouter>
+      </>
+    )
+}
+
+export default Main
